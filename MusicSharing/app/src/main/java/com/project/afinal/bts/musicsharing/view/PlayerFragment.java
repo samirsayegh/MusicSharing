862313a63b93b0b8ecaab512b19e5b52d81f7efb
Vero <lopez@strands.com>
2016-07-05 14:42:25 +0200

    Adding icons and loading values on view

--- MusicSharing/app/src/main/java/com/project/afinal/bts/musicsharing/view/PlayerFragment.java
+++ MusicSharing/app/src/main/java/com/project/afinal/bts/musicsharing/view/PlayerFragment.java
@@ -1,6 +1,8 @@
 package com.project.afinal.bts.musicsharing.view;
 
 import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.project.afinal.bts.musicsharing.R;
 import com.project.afinal.bts.musicsharing.view.base.BaseFragment;
@@ -10,12 +12,35 @@ import com.project.afinal.bts.musicsharing.view.base.BaseFragment;
  */
 public class PlayerFragment extends BaseFragment {
 
+    private TextView songQuestion;
+    private ImageView playerIcon;
+    private TextView songTitle;
+    private TextView songArtist;
+    private ImageView playerDislike;
+    private ImageView playerLike;
+
     public PlayerFragment() {
         layoutId = R.layout.fragment_player_view;
     }
 
     @Override
     protected void loadViewItems() {
-        //feelingsGrid = (GridView) rootView.findViewById(R.id.fragment_feelings_gv);
+        songQuestion = (TextView) rootView.findViewById(R.id.fragment_player_view_song_question_tv);
+        playerIcon = (ImageView) rootView.findViewById(R.id.fragment_player_view_song_play_iv);
+        songTitle = (TextView) rootView.findViewById(R.id.fragment_player_view_song_artist_song_tv);
+        songArtist = (TextView) rootView.findViewById(R.id.fragment_player_view_song_artist_name_tv);
+        playerDislike = (ImageView) rootView.findViewById(R.id.fragment_player_view_song_dislike_iv);
+        playerLike = (ImageView) rootView.findViewById(R.id.fragment_player_view_song_like_iv);
+    }
+
+    @Override
+    protected void loadValues() {
+        songQuestion.setText("What do you think about this song?");
+        songTitle.setText("This is my song");
+        songArtist.setText("John Doe");
+
+        playerIcon.setImageResource(R.drawable.ic_play_circle_outline_white_48dp);
+        playerDislike.setImageResource(R.drawable.dislike);
+        playerLike.setImageResource(R.drawable.like);
     }
 }
